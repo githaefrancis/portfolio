fe2b4f939151aeb77e4acb5e63173e9991dc25bc
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-07 21:09:30 +0300

    add event listener for about section menu buttons

--- js/index.js
+++ js/index.js
@@ -1,61 +1,53 @@
 $(() => {
   $(".header").height($(window).height());
   //make nav bar dark on refresh
-  if($(window).scrollTop()>150){
-    $('nav').addClass('bg-dark');
+  if ($(window).scrollTop() > 150) {
+    $("nav").addClass("bg-dark");
   }
-// make nav bar dark on scroll down
-  $(document).scroll(()=>{
-    const scrollLevel=$(window).scrollTop();
+
+  // make nav bar dark on scroll down
+  $(document).scroll(() => {
+    const scrollLevel = $(window).scrollTop();
     console.log($(window).scrollTop());
-    if(scrollLevel>150){
-      $('nav').addClass('bg-dark');
+    if (scrollLevel > 150) {
+      $("nav").addClass("bg-dark");
+    } else {
+      $("nav").removeClass("bg-dark");
     }
-    else{
-      $('nav').removeClass('bg-dark');
-    }
-  })
+  });
 });
 
-var projectsList=[];
+var projectsList = [];
 
 // import a list of projects
 
-  try{
-    fetch('js/projects.json')
-    .then(response=>response.json())
-    .then(data=>setProjectList(data))
-    }  
-    
-  
-  catch{
-  
-    projectsList = require('./projects.json');
-  
-    }
-    
-
-
-const setProjectList=(data)=>{
-  console.log(data)
-  projectsList=data;
-  console.log(projectsList);
-  console.log(projectsList.length);
-  projectsList.forEach(element => {
-    console.log(4);
-    console.log(element);
-    let id=element["id"];
-    let liveLink=element["github"];
-    let img=element["path"];
-    let projectName=element["name"];
-    let description=element["description"];
-    let languages=element["languages"];
-    if(id!==null){
-    createProject(id,img,projectName,description,liveLink);
-    }
-  });
+try {
+  fetch("js/projects.json")
+    .then((response) => response.json())
+    .then((data) => setProjectList(data));
+} catch {
+  projectsList = require("./projects.json");
 }
 
+const setProjectList = (data) => {
+  console.log(data);
+  projectsList = data;
+  console.log(projectsList);
+  console.log(projectsList.length);
+  projectsList.forEach((element) => {
+    console.log(4);
+    console.log(element);
+    let id = element["id"];
+    let liveLink = element["github"];
+    let img = element["path"];
+    let projectName = element["name"];
+    let description = element["description"];
+    let languages = element["languages"];
+    if (id !== null) {
+      createProject(id, img, projectName, description, liveLink);
+    }
+  });
+};
 
 // function to create an element
 const makeElement = (element) => {
@@ -68,7 +60,7 @@ const addAttribute = (element, attribute, value) => {
 };
 
 //create a project card
-const createProject = (id,imgUrl,title,description,liveLink) => {
+const createProject = (id, imgUrl, title, description, liveLink) => {
   let columnDiv = makeElement("div");
   addAttribute(columnDiv, "class", "col-12 col-lg-4 col-md-4 col-sm-12");
   let card = addAttribute(makeElement("div"), "class", "card");
@@ -77,14 +69,14 @@ const createProject = (id,imgUrl,title,description,liveLink) => {
   projectImage.src = imgUrl;
   projectImage.className = "img-fluid";
   let cardBody = addAttribute(makeElement("div"), "class", "card-body");
-  let cardTitle=addAttribute(makeElement("div"),"class","card-title");
-  let titleText=document.createTextNode(title);
+  let cardTitle = addAttribute(makeElement("div"), "class", "card-title");
+  let titleText = document.createTextNode(title);
   let cardDesc = addAttribute(makeElement("div"), "class", "card-desc");
   let cardFooter = addAttribute(makeElement("div"), "class", "card-footer");
   let cardDescParagraph = makeElement("p");
-  let footerButton=addAttribute(makeElement("a"), "class", "btn btn-primary");
-  let buttontext=document.createTextNode("Visit");
-  addAttribute(footerButton,"href",liveLink);
+  let footerButton = addAttribute(makeElement("a"), "class", "btn btn-primary");
+  let buttontext = document.createTextNode("Visit");
+  addAttribute(footerButton, "href", liveLink);
   cardTitle.appendChild(titleText);
   footerButton.appendChild(buttontext);
   cardFooter.appendChild(footerButton);
@@ -102,3 +94,27 @@ const createProject = (id,imgUrl,title,description,liveLink) => {
   columnDiv.appendChild(card);
   document.querySelector("#projects").appendChild(columnDiv);
 };
+
+// toggle about section content
+
+const menu = document.querySelector("#about-menu");
+menu.addEventListener("click", (event) => {
+  const isBtn = event.target.nodeName === "BUTTON";
+
+  if (!isBtn) {
+    return;
+  } else {
+    let btnId = event.target.id;
+    if (btnId === "profile") {
+      console.log("Iam profile");
+    } else if(btnId==="education"){
+      console.log("I am education");
+    }
+    else if(btnId==="interests"){
+      console.log("I am interests");
+    }
+    else{
+      console.log("nothing");
+    }
+  }
+});
